825e1c3e747a4bd972b2ff12902f3dcaf4342830
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2025-03-12 16:33:24 +0530

    Initial commit

---
+++ Backend/Cards/src/test/java/com/banking/system/Cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.system.Cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Backend/Transaction/src/test/java/com/banking/system/Transaction/TransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.system.Transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
