3040b706ac262b91d387fce123a62c16b0c8effb
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2026-01-02 03:27:10 +0530

    new e2e test

---
+++ e2e/src/tests/ui/pages/login.page.ts
@@ -0,0 +1,18 @@
+import {Page} from '@playwright/test';
+export class LoginPage{
+    readonly page: Page;
+    readonly usernameInput;
+    readonly passwordInput;
+    readonly loginButton;
+    constructor(page:Page){
+        this.page = page;
+        this.usernameInput = page.locator('input[name="username"]');
+        this.passwordInput = page.locator('input[name="password"]');
+        this.loginButton = page.locator('button[type="submit"]');
+    }
+    async login(username:string, password:string){
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+    }
+}
