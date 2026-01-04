376087e6e339e7c9d430619af8c1ef939b7cd2ce
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2026-01-04 22:04:50 +0530

    api tests

--- playwright-tests/src/tests/api/transfer/endpoints.ts
+++ playwright-tests/src/tests/api/transfer/endpoints.ts
@@ -1,6 +1,11 @@
 export const TransferEndpoints = {
-  TRANSFER_FUNDS: '/Account/transferFunds',
-  GET_MY_ACCOUNTS: '/Account/getMyAccounts',
+  GET_USER_ACCOUNTS_ADMIN: (username: string) =>
+    `/Account/getUserAccounts/${username}`,
+
+  CREATE_ACCOUNT: '/Account/createAccount',
+
   GET_MY_BALANCE: (accountId: number) =>
     `/Account/getMyAccountBalance/${accountId}`,
+
+  TRANSFER_FUNDS: '/Account/transferFunds',
 };
